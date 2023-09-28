ef538faeaffdecd2b6e8c950ca798017953c9727
Arenas García Juan Luis <144499294+IEVN-1001-15001478@users.noreply.github.com>
2023-09-27 18:46:19 -0600

    27/09/23 JLAG AgregamosComponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { CalculosComponent } from './formularios/calculos/calculos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -14,11 +16,13 @@ import { OperasBasComponent } from './formularios/operas-bas/operas-bas.componen
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    CalculosComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
